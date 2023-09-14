35cb581f72dbafadf61f1c8921d9b83a517f4bb3
Emma Antonucci <emmanicole0920@gmail.com>
2023-09-14 16:43:12 -0600

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -4,11 +4,12 @@ Starter file
 import random
 from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow
+
 def wordle():
-def enter_action(s):
-gw.show_message("You have to implement this method.")
-gw = WordleGWindow()
-gw.add_enter_listener(enter_action)
+    def enter_action(s):
+        gw.show_message("You have to implement this method.")
+    gw = WordleGWindow()
+    gw.add_enter_listener(enter_action)
 # Startup code
 if __name__ == "__main__":
-wordle()
+    wordle()
