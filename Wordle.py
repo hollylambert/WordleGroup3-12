cd2d75a36e7603d79427407d494c165ec5102487
Emma Antonucci <emmanicole0920@gmail.com>
2023-09-14 16:59:35 -0600

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -3,10 +3,21 @@ Starter file
 """
 import random
 from WordleDictionary import FIVE_LETTER_WORDS
-from WordleGraphics import WordleGWindow
+from WordleGraphics import WordleGWindow, N_ROWS, N_COLS
 
 def wordle():
     def enter_action(s):
+        #Generates a random index to choose a word from the FIVE_LETTER_WORDS array
+        index = random.randint(0, len(FIVE_LETTER_WORDS) - 1)
+        
+        #randomly selects a letter from FIVE_LETTER_WORDS using the randomly selected index
+        random_word = FIVE_LETTER_WORDS[index]
+
+        #Displays the random 
+        for i, letter in enumerate(random_word):
+            if i < N_COLS:
+                gw.set_square_letter(0, i, letter)
+
         gw.show_message("You have to implement this method.")
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
