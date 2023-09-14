b62e61982b9b11868ff0d20c10e0220587931e0d
hollylambert <95377820+hollylambert@users.noreply.github.com>
2023-09-14 14:26:37 -0600

    Create Wordle.py

---
+++ Wordle.py
@@ -0,0 +1,14 @@
+"""
+Starter file
+"""
+import random
+from WordleDictionary import FIVE_LETTER_WORDS
+from WordleGraphics import WordleGWindow
+def wordle():
+def enter_action(s):
+gw.show_message("You have to implement this method.")
+gw = WordleGWindow()
+gw.add_enter_listener(enter_action)
+# Startup code
+if __name__ == "__main__":
+wordle()
