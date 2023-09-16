cf7bb6ec103e44cd51f93ae2a1be4658c078f3a9
Emma Antonucci <emmanicole0920@gmail.com>
2023-09-16 12:02:57 -0600

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -7,6 +7,14 @@ from WordleGraphics import WordleGWindow, N_ROWS, N_COLS
 
 def wordle():
     def enter_action(s):
+
+        entered_word = s.lower()
+
+        if entered_word in FIVE_LETTER_WORDS:
+            gw.show_message("That word is valid!")
+        else:
+            gw.show_message("Not in word list")
+
         #Generates a random index to choose a word from the FIVE_LETTER_WORDS array
         index = random.randint(0, len(FIVE_LETTER_WORDS) - 1)
         
@@ -14,11 +22,11 @@ def wordle():
         random_word = FIVE_LETTER_WORDS[index]
 
         #Displays the random 
-        for i, letter in enumerate(random_word):
-            if i < N_COLS:
-                gw.set_square_letter(0, i, letter)
+        # for i, letter in enumerate(random_word):
+            # if i < N_COLS:
+                # gw.set_square_letter(0, i, letter)
 
-        gw.show_message("You have to implement this method.")
+        # gw.show_message("You have to implement this method.")
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
 # Startup code
